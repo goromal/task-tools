b163473ab5cc0ceb4c278d07cbd9f827a9727188
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-06-17 14:08:31 -0700

    retry token refresh automatically

--- task_tools/auth.py
+++ task_tools/auth.py
@@ -1,5 +1,6 @@
 import os
 from google.auth.transport.requests import Request
+from google.auth.exceptions import RefreshError
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -10,7 +11,12 @@ def getTaskService(task_secrets_file, task_refresh_token, task_scope):
         creds = Credentials.from_authorized_user_file(task_refresh_token, task_scope)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            try:
+                creds.refresh(Request())
+            except RefreshError:
+                os.remove(task_refresh_token)
+                flow = InstalledAppFlow.from_client_secrets_file(task_secrets_file, task_scope)
+                creds = flow.run_local_server(port=0)
         else:
             flow = InstalledAppFlow.from_client_secrets_file(task_secrets_file, task_scope)
             creds = flow.run_local_server(port=0)
