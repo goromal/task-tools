3f90c965ee7012768ee785426657b6fedb32ed9f
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-01-21 11:37:05 -0800

    add grader, delete commands (#4)

--- task_tools/cli.py
+++ task_tools/cli.py
@@ -1,5 +1,6 @@
 import click
-from datetime import datetime
+import datetime
+import os
 
 from task_tools.defaults import TaskToolsDefaults as TTD
 from task_tools.manage import TaskManager
@@ -23,6 +24,14 @@ from task_tools.manage import TaskManager
     help="Google Tasks refresh file (if it exists).",
 )
 @click.option(
+    "--task-list-id",
+    "task_list_id",
+    type=str,
+    default=TTD.TASK_LIST_ID,
+    show_default=True,
+    help="UUID of the Task List to query.",
+)
+@click.option(
     "--enable-logging",
     "enable_logging",
     type=bool,
@@ -30,30 +39,70 @@ from task_tools.manage import TaskManager
     show_default=True,
     help="Whether to enable logging.",
 )
-def cli(ctx: click.Context, task_secrets_file, task_refresh_token, enable_logging):
+def cli(ctx: click.Context, task_secrets_file, task_refresh_token, task_list_id, enable_logging):
     """Manage Google Tasks."""
     try:
-        ctx.obj = TaskManager(task_secrets_file=task_secrets_file, task_refresh_token=task_refresh_token, enable_logging=enable_logging)
+        ctx.obj = TaskManager(task_secrets_file=task_secrets_file, task_refresh_token=task_refresh_token, task_list_id=task_list_id, enable_logging=enable_logging)
     except Exception as e:
         print(f"Program error: {e}")
         exit(1)
 
 @cli.command()
 @click.pass_context
+@click.argument(
+    "filter",
+    type=str,
+)
 @click.option(
     "--date",
     "date",
     type=click.DateTime(formats=["%Y-%m-%d"]),
-    default=datetime.today(),
+    default=str(datetime.date.today()),
     show_default=True,
     help="Maximum due date for filtering tasks.",
 )
-def list(ctx: click.Context, date):
-    """List pending tasks."""
+@click.option(
+    "--no-ids",
+    "no_ids",
+    is_flag=True,
+    help="Don't show the UUIDs.",
+)
+def list(ctx: click.Context, filter, date, no_ids):
+    """List pending tasks according to a filter ∈ [all, p0, p1, p2, late]."""
     tasks = ctx.obj.getTasks(date)
-    for task in tasks:
-        print(task)
-        print()
+    if filter == "all":
+        filtered_tasks = tasks
+    elif filter == "p0":
+        filtered_tasks = [task for task in tasks if task.timing == 0 and task.days_late == 0]
+    elif filter == "p1":
+        filtered_tasks = [task for task in tasks if task.timing == 1 and task.days_late == 0]
+    elif filter == "p2":
+        filtered_tasks = [task for task in tasks if task.timing == 2 and task.days_late == 0]
+    elif filter == "late":
+        filtered_tasks = [task for task in tasks if task.timing >= 0 and task.days_late > 0]
+    elif filter == "":
+        print("ERROR: no list filter provided.")
+        exit(1)
+    else:
+        print(f"ERROR: unrecognized filter provided ({filter})")
+        exit(1)
+    for task in filtered_tasks:
+        print(f"- {task.toString(not no_ids)}")
+
+@cli.command()
+@click.pass_context
+@click.argument(
+    "task_id",
+    type=str,
+)
+def delete(ctx: click.Context, task_id):
+    """Delete a particular task by UUID."""
+    try:
+        ctx.obj.deleteTask(task_id)
+    except Exception as e:
+        print(f"Program error: {e}")
+        exit(1)
+    print(f"Task {task_id} deleted.")
 
 @cli.command()
 @click.pass_context
@@ -76,13 +125,106 @@ def list(ctx: click.Context, date):
     "--date",
     "date",
     type=click.DateTime(formats=["%Y-%m-%d"]),
-    default=datetime.today(),
+    default=str(datetime.date.today()),
     show_default=True,
     help="Task due date.",
 )
 def put(ctx: click.Context, name, notes, date):
     """Upload a task."""
     ctx.obj.putTask(name, notes, date)
+
+@cli.command()
+@click.pass_context
+@click.option(
+    "--start-date",
+    "start_date",
+    type=click.DateTime(formats=["%Y-%m-%d"]),
+    default=str(datetime.date.today() - datetime.timedelta(days=7)),
+    show_default=True,
+    help="First day of the grading window.",
+)
+@click.option(
+    "--end-date",
+    "end_date",
+    type=click.DateTime(formats=["%Y-%m-%d"]),
+    default=str(datetime.date.today()),
+    show_default=True,
+    help="Last day of the grading window.",
+)
+@click.option(
+    "-o",
+    "--out",
+    "out_file",
+    type=click.Path(),
+    default=TTD.GRADER_OUTPUT_FILE,
+    show_default=True,
+    help="CSV file to generate the report in.",
+)
+@click.option(
+    "--dry-run",
+    "dry_run",
+    is_flag=True,
+    help="Do a dry run; no task deletions.",
+)
+def grader(ctx: click.Context, start_date, end_date, out_file, dry_run):
+    """Generate a CSV report of how consistently tasks have been completed within the specified window.
+    
+    Grading criteria:\n
+    - P0: ... tasks must be completed same day.\n
+    - P1: ... tasks must be completed within a week.\n
+    - P2: ... tasks must be completed within a month.
+
+    Deletion / failure criteria:\n
+    - P[0-2]: [T] ... tasks that have not be completed within the appropriate window.
+    """
+    with open(out_file, "a") as logfile:
+        tasks = ctx.obj.getTasks(end_date, start_date=start_date)
+        on_time_tasks = []
+        late_tasks = []
+        failed_tasks = []
+        for task in tasks:
+            if task.timing >= 0:
+                late = task.days_late > 0
+                failed = task.autogen and late
+                logfile.write(f"{task.id}|{task.due}|{task.name}|{task.days_late}|{failed}\n")
+                if late:
+                    if failed:
+                        failed_tasks.append((task.days_late, task.id, task.toString(False)))
+                    else:
+                        late_tasks.append((task.days_late, task.toString(False)))
+                else:
+                    on_time_tasks.append(task.toString(False))
+        if len(on_time_tasks) > 0:
+            sorted_on_time_tasks = sorted(on_time_tasks)
+            print("PENDING TASKS:")
+            for task in sorted_on_time_tasks:
+                print(f"- {task}")
+        else:
+            print("NO PENDING TASKS")
+        print()
+        if len(late_tasks) > 0:
+            sorted_late_tasks = sorted(late_tasks, key=lambda k: -k[0])
+            print("LATE TASKS:")
+            for _, task in sorted_late_tasks:
+                print(f"- {task}")
+        else:
+            print("NO LATE TASKS")
+        print()
+        if len(failed_tasks) > 0:
+            sorted_failed_tasks = sorted(failed_tasks, key=lambda k: -k[0])
+            print("FAILED TASKS:")
+            for _, _, task in sorted_failed_tasks:
+                print(f"- {task}")
+            if not dry_run:
+                print("\nDeleting failed tasks...")
+                for _, task_id, _ in sorted_failed_tasks:
+                    try:
+                        ctx.obj.deleteTask(task_id)
+                    except Exception as e:
+                        print(f"WARNING: {e}")
+                        continue
+        else:
+            print("NO FAILED TASKS")
 
 def main():
     cli()
--- task_tools/manage.py
+++ task_tools/manage.py
@@ -5,23 +5,56 @@ from datetime import datetime, timedelta
 from easy_google_auth.auth import getGoogleService
 from task_tools.defaults import TaskToolsDefaults as TTD
 
+def dateTimeToGoogleDate(date_time):
+    return f"{date_time.strftime('%Y-%m-%d')}T23:59:59.000Z"
+
+def googleDateToDateTime(google_date):
+    return datetime.strptime(google_date.split("T")[0], "%Y-%m-%d")
+
 class Task(object):
     def __init__(self, data):
+        self.id = data["id"]
         self.name = data["title"]
         self.due = data["due"].split("T")[0]
+        due_date = googleDateToDateTime(data["due"])
+        if self.name[:3] == "P0:":
+            self.timing = 0
+            self.autogen = ("[T]" in self.name)
+            self.days_late = max((datetime.today() - due_date).days, 0)
+        elif self.name[:3] == "P1:":
+            self.timing = 1
+            self.autogen = ("[T]" in self.name)
+            self.days_late = max((datetime.today() - due_date).days - 5, 0)
+        elif self.name[:3] == "P2:":
+            self.timing = 2
+            self.autogen = ("[T]" in self.name)
+            self.days_late = max((datetime.today() - due_date).days - 27, 0)
+        else:
+            self.timing = -1
+            self.autogen = False
+            self.days_late = 0
         self.notes = data["notes"].replace("\n", "\n    ") if "notes" in data else None
     
+    def toString(self, show_id=True):
+        if self.timing >= 0 and self.days_late > 0:
+            timed_info = f"[LATE {self.days_late} DAYS] "
+        else:
+            timed_info = ""
+        if show_id:
+            id_info = f" < {self.id} >"
+        else:
+            id_info = ""
+        return f"(Due {self.due}) {timed_info}'{self.name}'{id_info}"
+
     def __repr__(self):
-        base = f"(Due {self.due}) '{self.name}'"
-        if self.notes is not None:
-            base += f"\n    {self.notes}"
-        return base
+        return self.toString()
 
 class TaskManager(object):
     def __init__(self, **kwargs):
         self.enable_logging = TTD.getKwargsOrDefault("enable_logging", **kwargs)
         if self.enable_logging:
             logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+        self.task_list_id = TTD.getKwargsOrDefault("task_list_id", **kwargs)
         self.service = getGoogleService(
             "tasks",
             "v1",
@@ -33,11 +66,15 @@ class TaskManager(object):
         if self.service is None:
             raise Exception(f"Expired credentials at {TTD.getKwargsOrDefault('task_refresh_token', **kwargs)}")
 
-    def getTasks(self, date=None):
+    def getTasks(self, date=None, start_date=None):
         if date is None:
             date = datetime.today()
-        fdate = f"{(date + timedelta(1)).strftime('%Y-%m-%d')}T23:00:00.000Z"
-        results = self.service.tasks().list(tasklist="MDY2MzkyMzI4NTQ1MTA0NDUwODY6MDow", showCompleted=False, dueMax=fdate).execute()
+        fdate = dateTimeToGoogleDate(date + timedelta(days=1))
+        if start_date is None:
+            results = self.service.tasks().list(tasklist=self.task_list_id, maxResults=100, showCompleted=False, dueMax=fdate).execute()
+        else:
+            fmindate = dateTimeToGoogleDate(start_date - timedelta(days=1))
+            results = self.service.tasks().list(tasklist=self.task_list_id, maxResults=100, showCompleted=False, dueMin=fmindate, dueMax=fdate).execute()
         items = results.get('items', [])
         if not items:
             if self.enable_logging:
@@ -58,4 +95,7 @@ class TaskManager(object):
         }
         if self.enable_logging:
             logging.info(f"Creating task {name} (due {date})")
-        self.service.tasks().insert(tasklist="MDY2MzkyMzI4NTQ1MTA0NDUwODY6MDow", body=body).execute()
+        self.service.tasks().insert(tasklist=self.task_list_id, body=body).execute()
+    
+    def deleteTask(self, task_id):
+        self.service.tasks().delete(tasklist=self.task_list_id, task=task_id).execute()
