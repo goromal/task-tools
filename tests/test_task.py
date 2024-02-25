ebb7c0c032f55dce69dc32f87bd8bfb16d0e0f60
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-02-25 12:46:04 -0800

    Update test_task.py

--- tests/test_task.py
+++ tests/test_task.py
@@ -2,5 +2,12 @@ import pytest
 from task_tools.manage import Task
 
 class TestTask:
+    mockdata1 = {
+        "id": "FAKEID",
+        "title": "P0: Do something!",
+        "due": "2024-01-01T00:00:00.00",
+        "notes": "Some notes."
+    }
     def test_initialization(self):
-        assert False # TODO
+        task = Task(TestTask.mockdata1)
+        assert task.due == "2024-01-01"
