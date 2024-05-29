a2d203b3d1e5d412be5cce16bbdf993feb5a9381
Andrew Torgesen <atorgesen@anduril.com>
2024-05-29 10:14:52 -0700

    Consolidate google secrets

--- task_tools/defaults.py
+++ task_tools/defaults.py
@@ -1,11 +1,8 @@
 import os
 
 class TaskToolsDefaults:
-    TASK_SECRETS_FILE = os.path.expanduser("~/secrets/task/secrets.json")
-    TASK_REFRESH_TOKEN = os.path.expanduser("~/secrets/task/token.json")
-    TASK_SCOPE = [
-        "https://www.googleapis.com/auth/tasks"
-    ]
+    TASK_SECRETS_FILE = os.path.expanduser("~/secrets/google/client_secrets.json")
+    TASK_REFRESH_TOKEN = os.path.expanduser("~/secrets/google/refresh.json")
     TASK_LIST_ID = "MDY2MzkyMzI4NTQ1MTA0NDUwODY6MDow"
     GRADER_OUTPUT_FILE = os.path.expanduser("~/data/task_grades/log.csv")
     ENABLE_LOGGING = False
@@ -15,7 +12,6 @@ class TaskToolsDefaults:
         argname_mapping = {
             "task_secrets_file": TaskToolsDefaults.TASK_SECRETS_FILE,
             "task_refresh_token": TaskToolsDefaults.TASK_REFRESH_TOKEN,
-            "task_scope": TaskToolsDefaults.TASK_SCOPE,
             "enable_logging": TaskToolsDefaults.ENABLE_LOGGING,
             "task_list_id": TaskToolsDefaults.TASK_LIST_ID,
         }
